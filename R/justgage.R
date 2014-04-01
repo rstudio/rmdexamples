5dde95ef2ebae8d99dcf81736d0e7ae3b096d658
JJ Allaire <jj@rstudio.org>
2014-04-01 13:47:14 -0400

    add justgage example

---
+++ R/justgage.R
@@ -0,0 +1,62 @@
+
+#' @export
+justgage <- function(title, value, min, max, label = NULL, 
+                     width = 200, height = 160) {
+  
+  structure(class = "justgage", list(
+    title = title,
+    label = label,
+    value = value,
+    min = min,
+    max = max,
+    width = width,
+    height = height
+  ))
+  
+}
+
+#' @export
+knit_print.justgage <- function(x) {
+  
+  # create random/unique id to bind the div and script
+  id <- paste("justgage", as.integer(stats::runif(1, 1, 10000)), sep="-") 
+  
+  # generate html for the justgage
+  html <- paste("<div id='", id, "' ", 
+                     "style='width:", x$width, "px;",
+                            "height:", x$height, "px'>",
+                "</div>", 
+                "<script>",
+                "var g = new JustGage({",
+                " id: '", id, "',", 
+                " value: ", x$value, ",", 
+                " min: ", x$min, ",", 
+                " max: ", x$max, ",", 
+                " title: '", x$title, "',", 
+                " label: '", x$label, "'",
+                "});",
+                "</script>",
+                sep = "")
+  
+  # return html and dependencies
+  structure(html, class = "knit_asis", 
+            knit_meta = list(raphael_dependency(), justgage_dependency()))
+}
+
+justgage_dependency <- function() {
+  structure(class = "html_dependency", list(
+    name = "justgage",
+    version = "1.0.1",
+    path = system.file("www/libs/justgage-1.0.1", package = "RmdExamples"),
+    script = "justgage.1.0.1.min.js"
+  ))
+}
+
+raphael_dependency <- function() {
+  structure(class = "html_dependency", list(
+    name = "raphael",
+    version = "2.1.2",
+    path = system.file("www/libs/raphael-2.1.2", package = "RmdExamples"),
+    script = "raphael.js"
+  ))
+}

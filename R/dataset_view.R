19d9aac24b89b2e2d649c93f75b34be0379542d8
JJ Allaire <jj@rstudio.org>
2014-04-03 09:47:55 -0400

    add example of paramaterized report

---
+++ R/dataset_view.R
@@ -0,0 +1,15 @@
+
+
+#' @export
+dataset_view <- function(csv, output_file) {
+  
+  # get the path to the report
+  report <- system.file("reports/dataset-view/dataset-view.Rmd", 
+                        package = "rmdexamples")
+  
+  # render to the output file using the specified csv
+  rmarkdown::render(input = report, 
+                    output_file = output_file,
+                    params = list(data = csv), 
+                    envir = new.env())
+} 

231ef3c7f3ec8ae59c89c9de9fae51e26b9e63b8
JJ Allaire <jj@rstudio.org>
2014-04-17 13:44:45 -0400

    add kmeans_cluster widget

---
+++ R/kmeans_cluster.R
@@ -0,0 +1,43 @@
+
+#' @export
+kmeans_cluster <- function(dataset) { 
+  
+  require(shiny)  
+  
+  shinyApp(
+    ui = fluidPage(
+      wellPanel(fluidRow(
+        column(4, selectInput('xcol', 'X Variable', names(dataset))),
+        column(4, selectInput('ycol', 'Y Variable', names(dataset),
+                              selected=names(dataset)[[2]])),
+        column(4, numericInput('clusters', 'Cluster count', 3,
+                               min = 1, max = 9))
+      )),
+      fluidRow(
+        plotOutput('kmeans')  
+      )
+    ),
+    
+    server = function(input, output, session) {
+      
+      # Combine the selected variables into a new data frame
+      selectedData <- reactive({
+        dataset[, c(input$xcol, input$ycol)]
+      })
+      
+      clusters <- reactive({
+        kmeans(selectedData(), input$clusters)
+      })
+      
+      output$kmeans <- renderPlot({
+        par(mar = c(5.1, 4.1, 0, 1))
+        plot(selectedData(),
+             col = clusters()$cluster,
+             pch = 20, cex = 3)
+        points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+      })
+    },
+    
+    options = list(height = 550)
+  )
+}

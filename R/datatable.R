2855652bdcb74c7344c17e7a0104407d48483945
Joe Cheng <joe@rstudio.org>
2014-05-19 09:37:00 -0700

    Adapt to use new htmltools

--- R/datatable.R
+++ R/datatable.R
@@ -1,79 +1,61 @@
 
 #' @export
 datatable <- function(data, align = NULL) {
-  structure(class = "datatable", list(
-    data = data,
-    align = align
-  ))
-}
-
-#' @export
-print.datatable <- function(x, ...) {
-  htmltools::html_print(datatable_html(x), datatable_dependencies())
-}
-
-#' @export
-knit_print.datatable <- function(x, ...) {
-  htmltools::html_knit_print(datatable_html(x), datatable_dependencies())
-}
-
-datatable_html <- function(x) {
 
   # create random/unique id for the table
   id <- paste("datatable", as.integer(stats::runif(1, 1, 10000)), sep="-")
 
-  # generate an html version of the table that includes the id
-  data <- x$data
-  align = if (is.null(align <- x$align)) '' else {
-    sprintf(' align="%s"', c(l = 'left', c = 'center', r = 'right')[align])
+  colformat <- if (length(align) == 0) {
+    as.character(tags$td("%s"))
+  } else {
+    sapply(rep_len(align, ncol(data)), function(x) {
+      as.character(tags$td(align=x, "%s"))
+    })
   }
-  html <- paste(c(
-    sprintf("<table id = \"%s\">", id),
-    c('<thead>', '<tr>',
-      sprintf('<th>%s</th>', htmltools::html_escape(colnames(data))),
-      '</tr>', '</thead>'),
-    '<tbody>',
-    paste(
-      '<tr>',
-      apply(data, 1, function(z) {
-        z = htmltools::html_escape(z)
-        paste(sprintf('<td%s>%s</td>', align, z), collapse = '')
-      }),
-      '</tr>', sep = ''
+
+  # generate an html version of the table that includes the id
+  table <- tagList(
+    tags$table(id = id,
+      tags$thead(
+        tags$tr(
+          lapply(colnames(data), function(name) { tags$th(name) })
+        )
+      ),
+      tags$tbody(
+        HTML(paste(sep = "", collapse = "\n",
+          "<tr>",
+          apply(data, 1, function(row) {
+            paste(sprintf(colformat, row), collapse = "")
+          }),
+          "</tr>"
+        ))
+      )
     ),
-    '</tbody>',
-    '</table>'
-  ), sep = '', collapse = '')
-
-  # append the script which binds the datatable
-  html <- paste(html,
-                "<div>&nbsp;</div>",
-                "<script>",
-                "$(document).ready(function() {",
-                "$('#", id ,"').dataTable();",
-                "});",
-                "</script>", sep = "")
-
-  # return html
-  html
-}
-
-datatable_dependencies <- function() {
-  list(
-    htmltools::html_dependency(
-      name = "jquery",
-      version = "1.11.0",
-      path = system.file("www/libs/jquery-1.11.0", package = "rmdexamples"),
-      script = "jquery.min.js"
-    ),
-    htmltools::html_dependency(
-      name = "datatables",
-      version = "1.9.4",
-      path = system.file("www/libs/datatables-1.9.4", package = "rmdexamples"),
-      stylesheet = "css/jquery.dataTables.css",
-      script = "js/jquery.dataTables.min.js"
-    )
+    tags$div(HTML("&nbsp;")),
+    tags$script(HTML(
+      "$(document).ready(function() {",
+      sprintf("$('#%s').dataTable();", id),
+      "});"
+    ))
   )
+
+  browsable(attachDependencies(table, datatable_dependencies))
 }
+
+datatable_dependencies <- list(
+  htmltools::htmlDependency(
+    name = "jquery",
+    version = "1.11.0",
+    src = system.file("www/libs/jquery-1.11.0", package = "rmdexamples"),
+    script = "jquery.min.js"
+  ),
+  htmltools::htmlDependency(
+    name = "datatables",
+    version = "1.9.4",
+    src = system.file("www/libs/datatables-1.9.4", package = "rmdexamples"),
+    stylesheet = "css/jquery.dataTables.css",
+    script = "js/jquery.dataTables.min.js"
+  )
+)
 
 
--- R/justgage.R
+++ R/justgage.R
@@ -1,38 +1,49 @@
+#' @import htmltools
+NULL
 
 #' @export
-justgage <- function(title, value, min, max, label = NULL) {
-  structure(class = "justgage", list(
+justgage <- function(title, value, min, max, label = NULL,
+  width = NULL, height = NULL) {
+
+  jg <- structure(class = "justgage", list(
     title = title,
     label = label,
     value = value,
     min = min,
-    max = max
+    max = max,
+    width = width,
+    height = height
   ))
+  jg
 }
 
 #' @export
 print.justgage <- function(x, ...) {
-  htmltools::html_print(
-    justgage_html(x, 450, 350),
-    justgage_dependencies()
-  )
+  print(as.tags(x))
+}
+
+#' @export
+as.tags.justgage <- function(x) {
+  justgage_html(x, 450, 350)
 }
 
 #' @export
 knit_print.justgage <- function(x, ..., options) {
-  htmltools::html_knit_print(
-    justgage_html(x, options$out.width.px, options$out.height.px),
-    justgage_dependencies()
+  knit_print(justgage_html(x, options$out.width.px, options$out.height.px),
+    options=options, ...
   )
 }
 
-justgage_html <- function(x, width, height) {
+justgage_html <- function(x, defaultWidth, defaultHeight) {
 
   # create random/unique id to bind the div and script
   id <- paste("justgage", as.integer(stats::runif(1, 1, 10000)), sep="-")
 
+  width <- if (is.null(x$width)) defaultWidth else x$width
+  height <- if (is.null(x$height)) defaultHeight else x$height
+
   # create a style attribute for the width and height
-  style <- paste("width:", width, "px;height:", height, "px", sep = "")
+  style <- paste("width:", width, "px;height:", height, "px;", sep = "")
 
   # create a list representing the parameters to JustGage
   options <- list(id = id,
@@ -43,28 +54,31 @@ justgage_html <- function(x, width, height) {
                   label = x$label)
 
   # generate html for the justgage
-  html <- paste(
-    "<div id=\"", id, "\" ",  "style=\"", style, "\">", "</div>",
-    "<script>var g = new JustGage(", RJSONIO::toJSON(options), ");</script>",
-    sep = "")
+  html <- tagList(
+    tags$div(id = id, style = style),
+    tags$script(HTML("var g = new JustGage(", RJSONIO::toJSON(options), ");"))
+  )
 
   # return html
-  html
+  html <- attachDependencies(
+    html,
+    justgage_dependencies
+  )
+
+  browsable(html)
 }
 
-justgage_dependencies <- function() {
-  list(
-    htmltools::html_dependency(
-      name = "raphael",
-      version = "2.1.2",
-      path = system.file("www/libs/raphael-2.1.2", package = "rmdexamples"),
-      script = "raphael.js"
-    ),
-    htmltools::html_dependency(
-      name = "justgage",
-      version = "1.0.1",
-      path = system.file("www/libs/justgage-1.0.1", package = "rmdexamples"),
-      script = "justgage.1.0.1.min.js"
-    )
+justgage_dependencies <- list(
+  htmltools::htmlDependency(
+    name = "raphael",
+    version = "2.1.2",
+    src = system.file("www/libs/raphael-2.1.2", package = "rmdexamples"),
+    script = "raphael.js"
+  ),
+  htmltools::htmlDependency(
+    name = "justgage",
+    version = "1.0.1",
+    src = system.file("www/libs/justgage-1.0.1", package = "rmdexamples"),
+    script = "justgage.1.0.1.min.js"
   )
-}
+)
